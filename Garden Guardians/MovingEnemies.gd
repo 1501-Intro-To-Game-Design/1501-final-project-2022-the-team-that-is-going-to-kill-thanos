e0675e710968e832880502a0f50e74d7f14d86b1
Pufblon <fireballiscool13@gmail.com>
2022-03-11 16:10:08 -0500

    Basic Working Moving Enemies

--- Garden Guardians/MovingEnemies.gd
+++ Garden Guardians/MovingEnemies.gd
@@ -4,6 +4,7 @@ extends Node
 # var a = 2
 # var b = "text"
 export(PackedScene) var importPathScene
+export(PackedScene) var importEnemyScene
 var enemyPathManager = []
 
 # Called when the node enters the scene tree for the first time.
@@ -11,13 +12,18 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
+func _process(delta):
+	for i in enemyPathManager:
+		i[1].addToOffset(i[0].speed)
+		i[0].position = i[1].getPathLocation()
 
-func addPath():
+func addEnemyPath():
 	var pathToFollow = importPathScene.instance()
+	var enemy = importEnemyScene.instance()
+	enemy.position = pathToFollow.getPathLocation()
 	add_child(pathToFollow)
-	enemyPathManager.append(pathToFollow)
-	
+	add_child(enemy)
+	enemyPathManager.append([enemy, pathToFollow])
+
 func _on_EnemySpawn_timeout():
-	addPath()
+	addEnemyPath()
--- Garden Guardians/Path.gd
+++ Garden Guardians/Path.gd
@@ -4,24 +4,17 @@ extends Path2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export(PackedScene) var importEnemyScene
-var enemy = []
 
-func addEnemy():
-	var enemyToAdd = importEnemyScene.instance()
-	add_child(enemyToAdd)
-	enemyToAdd.position = $Pathway.position
-	enemy.append(enemyToAdd)
 #enemy.position = $Pathway.position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	for i in enemy:
-		$Pathway.set_offset($Pathway.get_offset() + i.speed)
-		i.position = $Pathway.position
+#func _process(delta):
+	#pass
+func addToOffset(addNum):
+	$Pathway.set_offset($Pathway.get_offset() + addNum)
 
-func _on_StartEnemy_timeout():
-	addEnemy()
+func getPathLocation():
+	return $Pathway.position
