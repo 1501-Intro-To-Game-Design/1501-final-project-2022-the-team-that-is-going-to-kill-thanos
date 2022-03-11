dc4afdd6604f7224ddb467d7bea3e7fbca2be39d
Pufblon <fireballiscool13@gmail.com>
2022-03-11 16:49:38 -0500

    Comments + delta time

--- Garden Guardians/Path.gd
+++ Garden Guardians/Path.gd
@@ -1,20 +1,17 @@
 extends Path2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-#enemy.position = $Pathway.position
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
+
+#Function to increment the Pathway pathFollow2D's offset by the given number
 func addToOffset(addNum):
 	$Pathway.set_offset($Pathway.get_offset() + addNum)
 
+#Function that returns the position of the Pathway pathFollow2D
 func getPathLocation():
 	return $Pathway.position
